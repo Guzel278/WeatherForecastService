8f7134465dc08c3ada9942791a2dfe2c3e94b1bf
Guzel Ganieva <guzel95@list.ru>
2021-06-05 19:00:38 +0100

    Add project files.

---
+++ WeatherForecastService/Models/WindWeather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Entities.Models
+{
+    public class WindWeather
+    {
+        public string City { get; set; }
+        public DateTime Date { get; set; }
+        public int Speed { get; set; }
+        public string Direction { get; set; }
+    }
+}
---
+++ WeatherForecastService/WeatherClient.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace WeatherForecastService
+{
+    public class WeatherClient
+    {
+        private readonly HttpClient httpClient;
+        private readonly ServiceSettings serviceSettings;
+
+        public WeatherClient(HttpClient httpClient, IOptions<ServiceSettings> options)
+        {
+            this.httpClient = httpClient;
+            serviceSettings = options.Value;
+        }
+
+        public record Weather(string description);
+
+        public record Main(decimal temp);
+
+        public record Forecast(Weather[] weather, Main main, long dt);
+
+        public record Speed(int speed);
+        public record Direction(string direction);
+        public record Wind(Speed speed, Direction direction, long dt);
+
+        public async Task<Forecast> GetCurrentWeatherAsync(string city)
+        {
+            var forecast = await httpClient.GetFromJsonAsync<Forecast>(
+                $"https://{serviceSettings.OpenWeatherHost}/data/2.5/weather?q={city}&appid={serviceSettings.ApiKey}&units=metric");
+
+            return forecast;
+        }
+
+        public async Task<Wind> GetWindWeatherAsync(string city)
+        {
+            var wind = await httpClient.GetFromJsonAsync<Wind>(
+                $"https://{serviceSettings.OpenWeatherHost}/data/2.5/weather?q={city}&appid={serviceSettings.ApiKey}&units=metric");
+
+            return wind;
+        }
+    }
+}
